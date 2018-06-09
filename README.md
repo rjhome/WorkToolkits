7639244008fa9bfaedcb5bba59af35b558a7d42a
rjhome <rjhome@163.com>
2018-06-09 23:16:16 +0800

    itsm add args(add & delete)

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # WorkToolkits
 
-## itsm
+## itsm 工单管理
   ```
   usage: itsm.py [-h] [-f [finish_status]] [-bt [begin_time]] [-et [end_time]]
-                 [-id [ID]]
+                 [-abt [accept_time]] [-aet [accept_time]] [-id [ID]]
                  operate_type
 
   positional arguments:
-    operate_type        s: show ; u: update
+    operate_type        s: show ; u: update ; a: add ; d: delete
 
   optional arguments:
     -h, --help          show this help message and exit
     -f [finish_status]  0: unfinished ; 1: finished
-    -bt [begin_time]    begin time : yyyymmdd (>=)
-    -et [end_time]      endtime : yyyymmdd (<)
+    -bt [begin_time]    begin_time : yyyymmdd (>=)
+    -et [end_time]      end_time : yyyymmdd (<)
+    -abt [accept_time]  accept_time (begin): yyyymmdd (>=) | 0 for today
+    -aet [accept_time]  accept_time (end): yyyymmdd (<)
     -id [ID]            update itsm id : ID
   ```
--- itsm.py
+++ itsm.py
@@ -3,14 +3,18 @@
 import argparse
 import prettytable
 import WorkToolkitDB
+import re
+import datetime
 
 
 #get opt
 parser = argparse.ArgumentParser()
-parser.add_argument('operate', metavar='operate_type', type=str, choices=['s','u'], default='s', help='s: show ; u: update')
+parser.add_argument('operate', metavar='operate_type', type=str, choices=['s','u','a','d'], default='s', help='s: show ; u: update ; a: add ; d: delete')
 parser.add_argument('-f', nargs='?', metavar='finish_status', type=str, choices=['0','1'], default=0, help='0: unfinished ; 1: finished' )
-parser.add_argument('-bt', nargs='?', metavar='begin_time', type=str, help='begin time : yyyymmdd (>=)')
-parser.add_argument('-et', nargs='?', metavar='end_time', type=str, help='endtime : yyyymmdd (<)')
+parser.add_argument('-bt', nargs='?', metavar='begin_time', type=str, help='begin_time : yyyymmdd (>=)')
+parser.add_argument('-et', nargs='?', metavar='end_time', type=str, help='end_time : yyyymmdd (<)')
+parser.add_argument('-abt', nargs='?', metavar='accept_time', type=str, help='accept_time (begin): yyyymmdd (>=) | 0 for today')
+parser.add_argument('-aet', nargs='?', metavar='accept_time', type=str, help='accept_time (end): yyyymmdd (<)')
 parser.add_argument('-id', nargs='?', metavar='ID', type=int, help='update itsm id : ID')
 myOpt = parser.parse_args()
 
@@ -25,7 +29,7 @@ pt.align["title"] = "l" # Left align city names
 
 
 
-def show():
+def _show():
 	"""show itsm task"""
 	query = myTaskSession.query(WorkToolkitDB.db.Task)
 
@@ -33,13 +37,25 @@ def show():
 	if myOpt.f:
 		query = query.filter(WorkToolkitDB.db.Task.finish_status == myOpt.f)
 
-	#filter time >= bt and < et
+	#filter versopm_time >= bt and < et
 	if myOpt.bt:
 		query = query.filter(WorkToolkitDB.db.Task.version_time >= myOpt.bt)
-
 	if myOpt.et:
 		query = query.filter(WorkToolkitDB.db.Task.version_time < myOpt.et)
-	
+
+	#filter accept_time >= abt < aet
+	if myOpt.abt:
+		if myOpt.abt == '0':
+			myOpt.abt = datetime.datetime.now().strftime('%Y%m%d')
+		query = query.filter(WorkToolkitDB.db.Task.accept_time >= myOpt.abt)
+	if myOpt.aet:
+		query = query.filter(WorkToolkitDB.db.Task.accept_time < myOpt.aet)
+
+	#filter id
+	if myOpt.aet:
+		query = query.filter(WorkToolkitDB.db.Task.accept_time < myOpt.aet)
+
+	#print(myOpt)
 	data = query.order_by('version_time').all()
 
 	for record in data:
@@ -50,7 +66,7 @@ def show():
 
 	return 0
 
-def update():
+def _update():
 	"""set itsm task finished"""
 	query = myTaskSession.query(WorkToolkitDB.db.Task)
 
@@ -62,29 +78,80 @@ def update():
 	if not myOpt.f:
 		myOpt.f = 1
 
-
+	
 	query.filter(WorkToolkitDB.db.Task.id == myOpt.id).update({'finish_status': myOpt.f})
 	myTaskSession.commit()
 
+	"""
 	data = query.filter(WorkToolkitDB.db.Task.id == myOpt.id).all()
 	for record in data:
 			#record_arr = record.to_array()
 			pt.add_row(record.to_array())
 
 	print(pt)
+	"""
+
+	return 0
+
+def _add():
+	"""add one itsm task"""
+	query = myTaskSession.query(WorkToolkitDB.db.Task)
+	myTask = WorkToolkitDB.db.Task()
+
+	addDataStr = input('输入记录 格式: "task_id","title","version_time","memo","sys" :\n    >')
+	addData = re.split('\s',addDataStr)
+
+	#create record
+	myTask.task_id = addData[0]
+	myTask.title = addData[1]
+	myTask.version_time = addData[2]
+	myTask.memo = addData[3]
+	myTask.sys = addData[4]
+
+	#init accept_time , finish_status
+	myTask.accept_time = datetime.datetime.now().strftime('%Y%m%d')
+	myTask.finish_status = 0
+
+	myTaskSession.add_all([myTask])
+	myTaskSession.commit()
 
 	return 0
 
 
-	#update id 
+def _delete():
+	"""add one itsm task"""
+	query = myTaskSession.query(WorkToolkitDB.db.Task)
+
+	delDataStr = input('输入记录 格式: "id1,id2,id3,id4" :\n    >')
+	delData = re.split(',',delDataStr)
+
+	if len(delData) == 0:
+		print('ERR: no deleting id input')
+		return 1
+
+	for myID in delData:
+		myTask = query.get(myID)
+		myTaskSession.delete(myTask)
+
+	
+	myTaskSession.commit()
+
+	return 0
+
 
 
 
 #main operate
 if myOpt.operate == 's':
-	show()
+	_show()
 elif myOpt.operate =='u':
-	update()
+	_update()
+elif myOpt.operate == 'a':
+	_add()
+elif myOpt.operate == 'd':
+	_delete()
+
+
 
 
 
