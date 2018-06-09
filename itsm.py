b4ca90d5876706eee4864bfdf44235b9b2ccfcdc
rjhome <rjhome@163.com>
2018-06-09 20:30:44 +0800

    using argparse for args parsing

--- itsm.py
+++ itsm.py
@@ -1,24 +1,101 @@
+# coding utf-8
+
+import argparse
 import prettytable
-import click
 import WorkToolkitDB
 
+
+#get opt
+parser = argparse.ArgumentParser()
+parser.add_argument('operate', metavar='operate_type', type=str, choices=['s','u'], default='s', help='s: show ; u: update')
+parser.add_argument('-f', nargs='?', metavar='finish_status', type=str, choices=['0','1'], default=0, help='0: unfinished ; 1: finished' )
+parser.add_argument('-bt', nargs='?', metavar='begin_time', type=str, help='begin time : yyyymmdd (>=)')
+parser.add_argument('-et', nargs='?', metavar='end_time', type=str, help='endtime : yyyymmdd (<)')
+parser.add_argument('-id', nargs='?', metavar='ID', type=int, help='update itsm id : ID')
+myOpt = parser.parse_args()
+
+
+#get db session
+myTaskSession = WorkToolkitDB.db.TaskSession().mySession
+
+#config prettytable
 itsmCols = ["id","task_id","title","accept_time","version_time","finish_status","memo","sys"]
 pt = prettytable.PrettyTable(itsmCols)
 pt.align["title"] = "l" # Left align city names
-myTaskSession = WorkToolkitDB.db.TaskSession()
 
 
 
+def show():
+	"""show itsm task"""
+	query = myTaskSession.query(WorkToolkitDB.db.Task)
 
-@click.command()
-@click.option('--catalog', default=0, prompt='query catalog [0/1/2/3] : ' , help='0:all/1:unfinished/2:finished/3:nextweek unfinished' )
-def itsm_show(catalog):
-	data = myTaskSession.mySession.query(WorkToolkitDB.db.Task).order_by('version_time').all()
+	#filter finish_status 0/1/all
+	if myOpt.f:
+		query = query.filter(WorkToolkitDB.db.Task.finish_status == myOpt.f)
+
+	#filter time >= bt and < et
+	if myOpt.bt:
+		query = query.filter(WorkToolkitDB.db.Task.version_time >= myOpt.bt)
+
+	if myOpt.et:
+		query = query.filter(WorkToolkitDB.db.Task.version_time < myOpt.et)
+	
+	data = query.order_by('version_time').all()
+
 	for record in data:
-		#record_arr = record.to_array()
-		pt.add_row(record.to_array())
+			#record_arr = record.to_array()
+			pt.add_row(record.to_array())
 
-	click.echo(pt)
+	print(pt)
+
+	return 0
+
+def update():
+	"""set itsm task finished"""
+	query = myTaskSession.query(WorkToolkitDB.db.Task)
+
+	#ERR: not given itsm id for update 
+	if not myOpt.id:
+		print('Error: no itsm id given for update finish_status to 1')
+		return 1
+	#set default finsih_status if not given
+	if not myOpt.f:
+		myOpt.f = 1
 
 
-itsm_show()
+	query.filter(WorkToolkitDB.db.Task.id == myOpt.id).update({'finish_status': myOpt.f})
+	myTaskSession.commit()
+
+	data = query.filter(WorkToolkitDB.db.Task.id == myOpt.id).all()
+	for record in data:
+			#record_arr = record.to_array()
+			pt.add_row(record.to_array())
+
+	print(pt)
+
+	return 0
+
+
+	#update id 
+
+
+
+#main operate
+if myOpt.operate == 's':
+	show()
+elif myOpt.operate =='u':
+	update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
